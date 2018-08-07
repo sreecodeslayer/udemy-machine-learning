dfab361425dda17f7ea5bd32032fdf03f4df7f30
Sreenadh TC <kesav.tc8@gmail.com>
2018-08-07 13:08:12 +0530

    Repeated summarization and feature reduction to arrive at a 5% significance level of P value

--- 02_MultipleLinearRegression/multiple_linear_regression.py
+++ 02_MultipleLinearRegression/multiple_linear_regression.py
@@ -102,7 +102,7 @@ As per the summary, it is to be noted that the P value corresponding
 to the predictor variables (independent variables) : x2(0.99) > 0.05
 x2 here is the index 2, rmeove it from the optimal_X
 '''
-# Build an optimal matrix
+# Re-Build an optimal matrix
 optimal_X = sm_X[:, [0, 1, 3, 4, 5]]
 
 # Ordinary Least Squares (OLS)
@@ -139,5 +139,137 @@ Kurtosis:                       5.563   Cond. No.                     1.40e+06
 Warnings:
 [1] Standard Errors assume that the covariance matrix of the errors is correctly specified.
 [2] The condition number is large, 1.4e+06. This might indicate that there are
+strong multicollinearity or other numerical problems.
+"""
+'''
+As per the summary, it is to be noted that the P value corresponding
+to the predictor variables (independent variables) : x1(0.94) > 0.05
+x1 here is the index 1, rmeove it from the optimal_X
+'''
+# ============================================================================
+# Re-Build an optimal matrix
+optimal_X = sm_X[:, [0, 3, 4, 5]]
+
+# Ordinary Least Squares (OLS)
+sm_regressor = sm.OLS(endog=y, exog=optimal_X)
+sm_regressor = sm_regressor.fit()
+sm_regressor.summary()
+"""
+                            OLS Regression Results                            
+==============================================================================
+Dep. Variable:                      y   R-squared:                       0.951
+Model:                            OLS   Adj. R-squared:                  0.948
+Method:                 Least Squares   F-statistic:                     296.0
+Date:                Tue, 07 Aug 2018   Prob (F-statistic):           4.53e-30
+Time:                        12:56:57   Log-Likelihood:                -525.39
+No. Observations:                  50   AIC:                             1059.
+Df Residuals:                      46   BIC:                             1066.
+Df Model:                           3                                         
+Covariance Type:            nonrobust                                         
+==============================================================================
+                 coef    std err          t      P>|t|      [0.025      0.975]
+------------------------------------------------------------------------------
+const       5.012e+04   6572.353      7.626      0.000    3.69e+04    6.34e+04
+x1             0.8057      0.045     17.846      0.000       0.715       0.897
+x2            -0.0268      0.051     -0.526      0.602      -0.130       0.076
+x3             0.0272      0.016      1.655      0.105      -0.006       0.060
+==============================================================================
+Omnibus:                       14.838   Durbin-Watson:                   1.282
+Prob(Omnibus):                  0.001   Jarque-Bera (JB):               21.442
+Skew:                          -0.949   Prob(JB):                     2.21e-05
+Kurtosis:                       5.586   Cond. No.                     1.40e+06
+==============================================================================
+
+Warnings:
+[1] Standard Errors assume that the covariance matrix of the errors is correctly specified.
+[2] The condition number is large, 1.4e+06. This might indicate that there are
+strong multicollinearity or other numerical problems.
+"""
+'''
+As per the summary, it is to be noted that the P value corresponding
+to the predictor variables (independent variables) : x2(0.60) > 0.05
+x2 here is the index 4, remove it from the optimal_X
+(the actual dataset has to be taken here)
+'''
+# ============================================================================
+# Re-Build an optimal matrix
+optimal_X = sm_X[:, [0, 3, 5]]
+
+# Ordinary Least Squares (OLS)
+sm_regressor = sm.OLS(endog=y, exog=optimal_X)
+sm_regressor = sm_regressor.fit()
+sm_regressor.summary()
+"""
+                            OLS Regression Results                            
+==============================================================================
+Dep. Variable:                      y   R-squared:                       0.950
+Model:                            OLS   Adj. R-squared:                  0.948
+Method:                 Least Squares   F-statistic:                     450.8
+Date:                Tue, 07 Aug 2018   Prob (F-statistic):           2.16e-31
+Time:                        13:02:51   Log-Likelihood:                -525.54
+No. Observations:                  50   AIC:                             1057.
+Df Residuals:                      47   BIC:                             1063.
+Df Model:                           2                                         
+Covariance Type:            nonrobust                                         
+==============================================================================
+                 coef    std err          t      P>|t|      [0.025      0.975]
+------------------------------------------------------------------------------
+const       4.698e+04   2689.933     17.464      0.000    4.16e+04    5.24e+04
+x1             0.7966      0.041     19.266      0.000       0.713       0.880
+x2             0.0299      0.016      1.927      0.060      -0.001       0.061
+==============================================================================
+Omnibus:                       14.677   Durbin-Watson:                   1.257
+Prob(Omnibus):                  0.001   Jarque-Bera (JB):               21.161
+Skew:                          -0.939   Prob(JB):                     2.54e-05
+Kurtosis:                       5.575   Cond. No.                     5.32e+05
+==============================================================================
+
+Warnings:
+[1] Standard Errors assume that the covariance matrix of the errors is correctly specified.
+[2] The condition number is large, 5.32e+05. This might indicate that there are
+strong multicollinearity or other numerical problems.
+"""
+'''
+As per the summary, it is to be noted that the P value corresponding
+to the predictor variables (independent variables) : x2(0.060) is slightly larger 0.05
+x2 here is the index 5, you can either remove it from the optimal_X or keep it
+by providing a criteria to do so
+(the actual dataset has to be taken here)
+'''
+# ============================================================================
+# Re-Build an optimal matrix
+optimal_X = sm_X[:, [0, 3]]
+
+# Ordinary Least Squares (OLS)
+sm_regressor = sm.OLS(endog=y, exog=optimal_X)
+sm_regressor = sm_regressor.fit()
+sm_regressor.summary()
+"""
+                            OLS Regression Results                            
+==============================================================================
+Dep. Variable:                      y   R-squared:                       0.947
+Model:                            OLS   Adj. R-squared:                  0.945
+Method:                 Least Squares   F-statistic:                     849.8
+Date:                Tue, 07 Aug 2018   Prob (F-statistic):           3.50e-32
+Time:                        13:06:12   Log-Likelihood:                -527.44
+No. Observations:                  50   AIC:                             1059.
+Df Residuals:                      48   BIC:                             1063.
+Df Model:                           1                                         
+Covariance Type:            nonrobust                                         
+==============================================================================
+                 coef    std err          t      P>|t|      [0.025      0.975]
+------------------------------------------------------------------------------
+const       4.903e+04   2537.897     19.320      0.000    4.39e+04    5.41e+04
+x1             0.8543      0.029     29.151      0.000       0.795       0.913
+==============================================================================
+Omnibus:                       13.727   Durbin-Watson:                   1.116
+Prob(Omnibus):                  0.001   Jarque-Bera (JB):               18.536
+Skew:                          -0.911   Prob(JB):                     9.44e-05
+Kurtosis:                       5.361   Cond. No.                     1.65e+05
+==============================================================================
+
+Warnings:
+[1] Standard Errors assume that the covariance matrix of the errors is correctly specified.
+[2] The condition number is large, 1.65e+05. This might indicate that there are
 strong multicollinearity or other numerical problems.
 """
